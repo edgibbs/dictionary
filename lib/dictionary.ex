77b1460a63fa7e8e8af230a593f8f27c27df6e73
Ed Gibbs <edward_gibbs@yahoo.com>
2017-06-25 06:35:50 -0700

    Recfactor with pipeline operator

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -2,11 +2,12 @@ defmodule Dictionary do
   @file_system Application.get_env(:dictionary, :file_system, File)
 
   def random_word do
-    Enum.random(word_list)
+    word_list() |> Enum.random
   end
 
   def word_list do
-    contents = @file_system.read!("assets/words.txt")
-    list = String.split(contents, ~r/\n/)
+    "assets/words.txt"
+    |> @file_system.read!
+    |> String.split(~r/\n/)
   end
 end
--- lib/test_file_system.ex
+++ lib/test_file_system.ex
@@ -1,6 +1,5 @@
 defmodule Dictionary.TestFileSystem do
   def read!(_file) do
-    IO.puts("what")
     "not_so_random"
   end
 end
